651f72bcf033d3510c39b364c94bd13c83e90dd8
Nguyen Phan Van <nguyenpv1302@gmail.com>
2024-09-17 15:52:28 +0700

    Bai tap buoi 02

--- BaiTap/Program.cs
+++ BaiTap/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Bài tập buổi 02
+// Bài tập 1
+// Console.Write("Nhap vao so ngay: ");
+// string? soNgayNhap = Console.ReadLine();
+// int soNgay = int.Parse(soNgayNhap);
+// int soTuan = soNgay / 7;
+// int soNgayLe = soNgay % 7;
+// Console.WriteLine($"So tuan: {soTuan}, so ngay le: {soNgayLe}");
+
+// Bài tập 2
+// Console.Write("Nhap vao gia tri don hang: ");
+// string? giaTriDonHangNhap = Console.ReadLine();
+// double giaTriDonHang = int.Parse(giaTriDonHangNhap);
+// Console.Write("Nhap vao phan tram giam gia: ");
+// string? phanTramGiamGiaNhap = Console.ReadLine();
+// double phanTramGiamGia = int.Parse(phanTramGiamGiaNhap);
+// double soTienGiamGia = (giaTriDonHang * phanTramGiamGia) / 100;
+// double tongSoTienPhaiThanhToan = giaTriDonHang - soTienGiamGia;
+// Console.WriteLine($"So tien giam gia: {soTienGiamGia}, tong so tien phai tra: {tongSoTienPhaiThanhToan}");
+
+// Bài tập 3
+// Console.Write("Nhap vao so phut: ");
+// string soPhutNhap = Console.ReadLine();
+// int soPhut = int.Parse(soPhutNhap);
+// int soGioResult = soPhut / 60;
+// int soPhutResult = soPhut % 60;
+// Console.WriteLine($"So gio: {soGioResult}, so phut: {soPhutResult}");
+
+// Bài tập 4
+// Console.Write("Nhap vao so tien goc: ");
+// string soTienNhap = Console.ReadLine();
+// double soTien = double.Parse(soTienNhap);
+// Console.Write("Nhap vao VAT: ");
+// string vatNhap = Console.ReadLine();
+// double vat = double.Parse(vatNhap);
+// double tienThue = (soTien * vat) / 100;
+// double tongSoTien = soTien + vat;
+// Console.WriteLine($"Tong so tien: {tongSoTien}");
+
+// Bài tập 5
+// Console.Write("Nhap vao so tien USD: ");
+// string soTienNhap = Console.ReadLine();
+// double soTien = double.Parse(soTienNhap);
+// Console.Write("Nhap vao ty gia chuyen doi USD sang VND: ");
+// string tyGiaNhap = Console.ReadLine();
+// double tyGia = double.Parse(tyGiaNhap);
+// double tienVND = soTien * tyGia;
+// Console.WriteLine($"{soTien}USD = {tienVND}VND");
+#endregion
